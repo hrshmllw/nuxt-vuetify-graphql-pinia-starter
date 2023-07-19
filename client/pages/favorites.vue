7ed69415a99d0f5733cb193178c72df29a2ffee2
hrshmllw <edceljmacawili@gmail.com>
2023-07-19 18:48:09 +0800

    refactor(favorites): added if-else statement for no favorites

--- client/pages/favorites.vue
+++ client/pages/favorites.vue
@@ -6,13 +6,19 @@
 		</v-btn>
 		<h3 class="my-5">Favorite Rockets</h3>
 
-		<v-row>
-			<v-col v-for="rocket in favoriteRockets" :key="rocket" cols="12" md="6" lg="4">
-				<v-card>
-					<v-card-title>{{ rocket }}</v-card-title>
-				</v-card>
-			</v-col>
-		</v-row>
+		<template v-if="favoriteRockets.length === 0">
+			<div class="d-flex justify-center"><p>You haven't favorited any rockets yet.</p></div>
+		</template>
+
+		<template v-else>
+			<v-row>
+				<v-col v-for="rocket in favoriteRockets" :key="rocket" cols="12" md="6" lg="4">
+					<v-card>
+						<v-card-title>{{ rocket }}</v-card-title>
+					</v-card>
+				</v-col>
+			</v-row>
+		</template>
 	</v-container>
 </template>
 
